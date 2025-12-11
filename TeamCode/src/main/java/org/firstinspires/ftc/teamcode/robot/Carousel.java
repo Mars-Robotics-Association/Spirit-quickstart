641145c1fc5a2d5acea0ce6b79967cf42b85ebeb
Matt Hanson <sqlsourcerer@gmail.com>
2025-12-11 11:16:47 -0500

    make shooter configurable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Carousel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Carousel.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode.robot;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
 public class Carousel {
-
     private final Servo kickerServo;//lifts the teardrop to feed the artifact to the shooter
     private final Servo carouselServo;//turns the carousel
 
