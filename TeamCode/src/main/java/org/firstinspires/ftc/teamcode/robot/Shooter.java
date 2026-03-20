a3ef75b2000d0ad99a4773ce07de0b40861cfe13
Jonas Hanson <jonas.hanson@marsroboticsassociation.org>
2026-03-19 21:06:24 -0400

    Starting class materials

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.java
@@ -15,11 +15,6 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
  * spinning in opposite directions to launch balls, and a servo ({@code "tiltServo"}) to
  * adjust the launch angle for near vs. far targets.
  *
- * <p>Velocity control uses a feedforward + proportional feedback loop with exponential
- * smoothing (see {@link #setShooterVelocity}). The feedforward values
- * ({@link #feedLeftForward}, {@link #feedRightForward}) set a baseline power proportional
- * to the desired speed, while the feedback term corrects any error between the smoothed
- * target and actual velocities using gain {@link #kp}.
  *
  * <p>Fields annotated with {@code @Config} are tunable via FTC Dashboard.
  *
@@ -34,35 +29,11 @@ public class Shooter {
     static public double nearTiltPosition = .03;//for testing
     static public double farTiltPosition = .15;//for testing
     static public double homeTiltPosition = 0;//for testing
-    /* *****************BALLPARK VELOCITY OF ENCODED SHOOTER MOTORS
 
+    static public double shooterPower = 0;
+    static public double nearShooterPower = .3;//tps for use with encoders to set shooter speed
+    static public double farShooterPower = .425;//tps for use with encoders to set shooter speed
 
-/*The near target as measured by tachometer is between 1700 and 1900 rpm so say 1800 (as measured by Mr. Beckstead on 1/21/2026).
-The far target as measured by tachometer is between 3600 and 3700 rpm so say 3650
-
-So, velocity for near target = (1800 rotations per minute /28 ticks per seconds)/60 seconds = 840 tps
-So, velocity for far target = (3650 rotations per second/28 ticks per seconds)/60 seconds = 1,703 tps
-
-   */
-    static public double nearShooterVelocity = 650;//tps for use with encoders to set shooter speed
-    static public double farShooterVelocity = 945;//tps for use with encoders to set shooter speed
-
-    static public double shooterVelocity = 0;
-    static public double maxShooterVelocityLeft = 2350;//tps when power set to 1
-    static public double maxShooterVelocityRight = 2460;//tps when power set to 1
-
-    double smoothTargetShooterVelocity = 0;
-    double smoothActualLeftShooterVelocity = 0;
-    double smoothActualRightShooterVelocity = 0;
-    public static double actualMotorPower = 0;
-
-    static public double kp = 0.002;
-    static public double smoothingFactor = .1;
-    public double shooterPower = 1;
-    static public double speed = .2;
-
-    static public double feedLeftForward = .41;
-            static public double feedRightForward = .35;
     //*********************************************
 
     /**
@@ -94,53 +65,19 @@ So, velocity for far target = (3650 rotations per second/28 ticks per seconds)/6
      * computes a proportional correction term added to the static feedforward power.
      * If {@code shooterVelocity} is 0, both motors are stopped immediately.
      *
-     * @param shooterVelocity desired flywheel velocity in ticks per second
+     * @param shooterPower desired flywheel velocity in ticks per second
      * @param telemetry       telemetry instance for logging velocity diagnostics
      */
-    public void setShooterVelocity(double shooterVelocity, Telemetry telemetry) {
-        telemetry.addData("Target T/S", shooterVelocity);
-      // shooterMotorLeft.setVelocity(shooterVelocity);//if using encoders, use this code. Otherwise, set power
-      // shooterMotorRight.setVelocity(shooterVelocity);//if using encoders, use this code. Otherwise, set power
-//        shooterMotorLeft.setPower(shooterPower);
-//        shooterMotorRight.setPower(shooterPower);
+    public void setShooterPower(double shooterPower, Telemetry telemetry) {
+        shooterMotorLeft.setPower(shooterPower);
+        shooterMotorRight.setPower(shooterPower);
 
         //fail safe
-        if (shooterVelocity == 0){
+        if (shooterPower == 0){
            shooterMotorLeft.setPower(0);
            shooterMotorRight.setPower(0);
            return;
        }
-        //convert target velocity to feed forward
-
-       // double feedLeftForward = shooterVelocity/maxShooterVelocityLeft;//target/max (moved to class variable)
-        //double feedRightForward = shooterVelocity/maxShooterVelocityRight;//target/max (moved to class variable)
-
-        //measure current velocity
-        telemetry.addData("FeedLeftForward ", feedLeftForward);
-        telemetry.addData("FeedRightForward ", feedRightForward);
-        double actualLeftVelocity = shooterMotorLeft.getVelocity();//removed mw's negative from shooterMotorLeft
-        double actualRightVelocity = shooterMotorRight.getVelocity();//removed mw's negative from shooterMotorRight
-
-        //calculate smoothing
-        smoothTargetShooterVelocity = (shooterVelocity * smoothingFactor) + (1 -  smoothingFactor) * smoothTargetShooterVelocity;
-        smoothActualLeftShooterVelocity = (actualLeftVelocity * smoothingFactor) + (1 -  smoothingFactor) * smoothActualLeftShooterVelocity;
-        smoothActualRightShooterVelocity = (actualRightVelocity * smoothingFactor) + (1 -  smoothingFactor) * smoothActualRightShooterVelocity;
-
-        //how fast am I going versus how fast I want to go
-        double feedbackLeft = (smoothTargetShooterVelocity - smoothActualLeftShooterVelocity) * kp;
-        double feedbackRight = (smoothTargetShooterVelocity - smoothActualRightShooterVelocity) * kp;
-
-        telemetry.addData("FeedbackLeft", feedbackLeft);
-        telemetry.addData("FeedbackRight", feedbackRight);
-
-        //set the power of the shooter motors to their current speed plus or minus the feed foward factors
-        //so it is constantly adjusting itself to the target
-      //shooterMotorLeft.setPower(feedbackLeft + feedLeftForward);
-        actualMotorPower = (feedbackLeft + feedLeftForward) * 1000;
-        shooterMotorLeft.setPower(feedbackLeft + feedLeftForward);
-       shooterMotorRight.setPower(feedbackRight + feedRightForward);
-        //shooterMotorLeft.setPower(speed);
-       // shooterMotorRight.setPower(speed);
     }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/SpiritTeleop2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/SpiritTeleop2.java
@@ -42,7 +42,8 @@ import org.firstinspires.ftc.teamcode.Drawing;
 @TeleOp(name = "SpiritTeleop2", group = "Teleop")
 public class SpiritTeleop2 extends LinearOpMode {
     public double launchSequenceTimer = 0;
-    //public int counter = 0;//variable to use to cause a waiting period in the launch sequence
+    //public int counter = 0;//This variable might be used in the programming class if
+    // a student rewrites the launch sequence using a loop instead of a state engine
     int launchStep = 0;
     double stepStartTime = 0;
     static public double defaultLaunchStepDelay = .6;//
@@ -56,7 +57,6 @@ public class SpiritTeleop2 extends LinearOpMode {
     //-------------------------------------------
     @Override
     public void runOpMode() {
-//      telemetry.setAutoClear(false);
         telemetry.clear();
         double rampUpTimer = 0;
         double driveTimer = 0;
@@ -82,186 +82,8 @@ public class SpiritTeleop2 extends LinearOpMode {
 
         while (opModeIsActive()){
 
-
-
-//LIFT ROBOT OFF OF MAT****************************************************
-
-            if (gamepad1.back){
-                lift.engageLift();
-            }
-
-            //reset lift position
-            if (gamepad1.dpad_left) {
-                lift.homeLift();
-            }
-//END CODE FOR LIFT*********************************************************
-
-            telemetry.addData("Test", 0);
-            //Operate Intake: left bumper is intake and right bumper is eject
-            if (gamepad2.left_bumper) {
-                intake.setPower(1);
-            } else if (gamepad2.right_bumper) {
-                intake.setPower(-1);//sets the power on the intake motor based on the values from the bumps
-            } else {
-                intake.setPower(0);
-            }
-
-//JUST FOR TESTING POSITON OF KICKER SERVO-------
-
-            if (gamepad2.b) {
-                carousel.setHomePositionKicker();
-            }
-            if (gamepad2.x) {
-                carousel.setTinyKicker();
-            }
-            if (gamepad2.y) {
-                carousel.setFullKicker();
-            }
-//JUST FOR TESTING POSITION OF TILT SERVO
-            if (gamepad1.a) {
-                shooter.setNearTiltPosition(shooter.nearTiltPosition);
-            }
-
-            if (gamepad1.b) {
-                shooter.setFarTiltPosition(shooter.farTiltPosition);
-            }
-
-            if (gamepad1.x) {
-                shooter.setHomeTiltPosition();
-            }
-
-//END CODE FOR TESTING POSITION OF TILT SERVO************************************
-
-//JUST FOR TESTING POSITION OF CAROUSEL********************************************
-            //if (gamepad2.dpad_left) {
-            //carousel.spinCarouselMin();//this is 0
-
-            //}
-            if (gamepad2.dpad_left) {
-                carousel.spinCarouselHome();//
-
-            }
-            // if (gamepad2.dpad_up) {
-            // carousel.spinCarouselMax();//this is 1
-
-            //}
-            if (gamepad2.dpad_up) {
-                carousel.spinCarouselIntakeOne();//
-
-            }
-
-            if (gamepad2.dpad_right) {
-                carousel.spinCarouselIntakeTwo();//
-
-            }
-
-            if (gamepad2.dpad_down) {
-                carousel.spinCarouselIntakeThree();//
-
-            }
-
-            if (gamepad1.dpad_up) {
-                carousel.spinCarouselLaunchOne();
-
-            }
-            if (gamepad1.dpad_right) {
-                carousel.spinCarouselLaunchTwo();
-            }
-
-            if (gamepad1.dpad_down) {
-                carousel.spinCarouselLaunchThree();
-            }
-//--------------------------END TESTING OF CAROUSEL
-
-
-
-// ---------------- INTAKE + CAROUSEL SEQUENCE (GAMEPAD 1 RIGHT TRIGGER) ----------------
-                /*
-                This section of the code is an intake sequence. It cycles 3 times.  Upon pulling the trigger
-                on gamepad 1, the intake begins spinning and the carousel spins to intakePositionOne.
-                On the second trigger pull, the intake spins and the carousel advances to intakePositionTwo.
-                On the third trigger pull, the intake spins and the carousel advances to intakePositionThree,
-                then resets for the next cycle.
-                */
-
-            boolean triggerPressed = gamepad1.right_trigger > 0.25;
-
-            // While trigger is held, keep intake running
-            if (triggerPressed) {
-                intake.setPower(intakePower);
-            }
-
-            // Detect NEW trigger pull (rising edge)
-            if (triggerPressed && !triggerHeld) {
-                triggerHeld = true;
-
-                // Advance carousel one position per pull
-                if (intakeStep == 0) {
-                    intake.setPower(intakePower);
-                    carousel.spinCarouselIntakeOne();
-                    intakeStep = 1;
-                } else if (intakeStep == 1) {
-                    intake.setPower(intakePower);
-                    carousel.spinCarouselIntakeTwo();
-                    intakeStep = 2;
-                } else if (intakeStep == 2) {
-                    intake.setPower(intakePower);
-                    carousel.spinCarouselIntakeThree();
-                    intakeStep = 0;
-                }
-
-
-            }
-
-            // Detect trigger release
-            if (!triggerPressed && triggerHeld) {
-                triggerHeld = false;
-                intake.setPower(0);   // Stop intake when released
-            }
-            //END INTAKE SEQUENCE---------------------------------------
-/**
-            //MANUAL LOAD-----------------------------
-            if (ADD CONTROL HERE) {
-                carousel.setHomePositionKicker();
-                carousel.setHomePositionKicker();
-                carousel.spinCarouselLaunchOne();
-                carousel.setTinyKicker();
-            }
-            //END MANUAL LOAD-----------------------------------------------
-*/
-            if (gamepad2.b) {
-                carousel.setHomePositionKicker();
-            }
-
-
-//  KILL BUTTON*****************************************************************
-if(gamepad2.back){
-currentState = State.IDLE;
-launchStep = 0;
-shooter.shooterMotorLeft.setPower(0);
-shooter.shooterMotorRight.setPower(0);
-}
-//END KILL BUTTON************************************************************
-
-
-/*
-//KILL CAROUSEL AND KICKER***************************************************************
-
-            if(gamepad1.left_bumper){
-            carousel.disableCarouselAndKicker();
-            currentState = State.IDLE;
-            NEED TO RESET THE CASE BACK TO 0 ALSO
-            carousel.enableCarouselAndKicker();
-            }
-
-//END KILL CAROUSEL AND KICKER**************************************************************
-
-*/
-
             //CODE FOR WHEN THE TRIGGERS ARE PRESSED----------------------------------
             switch (currentState) {
-
-
 
                 // ------------------------------------------------------
                 //  IDLE — waiting for trigger input
@@ -272,20 +94,17 @@ shooter.shooterMotorRight.setPower(0);
                         //shooter.shooterVelocity = shooter.nearShooterVelocity;
                         shooter.shooterMotorLeft.setPower(.3);
                         shooter.shooterMotorRight.setPower(.3);
-                        shooter.shooterVelocity = Shooter.nearShooterVelocity;
-                        shooter.setShooterVelocity(shooter.shooterVelocity, telemetry);
+                        shooter.shooterPower = shooter.nearShooterPower;
                         tiltPosition = shooter.nearTiltPosition;
                         rampUpTimer = getRuntime() + 2.0;   // 2-second spin-up
                         currentState = State.RAMPING;
-
                     }
 
                     // Far shot (left trigger)
                     if (gamepad2.left_trigger > 0.25 && gamepad2.right_trigger < 0.1){
                         shooter.shooterMotorLeft.setPower(.425);
                         shooter.shooterMotorRight.setPower(.425);
-                        shooter.shooterVelocity = Shooter.farShooterVelocity;
-                        shooter.setShooterVelocity(Shooter.shooterVelocity, telemetry);
+                        shooter.shooterPower = Shooter.farShooterPower;
                         tiltPosition = shooter.farTiltPosition;
                         rampUpTimer = getRuntime() + 2.0;
                         currentState = State.RAMPING;
@@ -296,18 +115,14 @@ shooter.shooterMotorRight.setPower(0);
                 //  RAMPING — waiting for flywheel to reach speed
                 // ------------------------------------------------------
                 case RAMPING:
-                    shooter.setShooterVelocity(shooter.shooterVelocity, telemetry);
+                    shooter.setShooterPower(shooter.shooterPower, telemetry);
                     if (getRuntime() > rampUpTimer) {
                         // Begin launch sequence
                         currentState = State.LAUNCHING;
                         launchStep = 0;
-                        //carousel.spinCarouselLaunchOne();
-
-
                         shooter.setHomeTiltPosition();//moved here from IDLE
                         carousel.setHomePositionKicker();//moved here from IDLE
                         stepStartTime = getRuntime();
-
                     }
                     break;
 
@@ -484,8 +299,9 @@ shooter.shooterMotorRight.setPower(0);
                     break;
             }
 
-            //------------------------END OF TRIGGER CONTROL-----------------------------------
+            //------------------------END OF LAUNCH SEQUENCE-----------------------------------
 
+            //CODE FOR DRIVING---------------------------------------------------------
             drive.setDrivePowers(new PoseVelocity2d(
                     new Vector2d(
                             -gamepad1.left_stick_y,
@@ -497,10 +313,135 @@ shooter.shooterMotorRight.setPower(0);
             drive.updatePoseEstimate();
 
             Pose2d pose = drive.localizer.getPose();
-           // telemetry.addData("x", pose.position.x);
-           // telemetry.addData("y", pose.position.y);
-           // telemetry.addData("heading (deg)", Math.toDegrees(pose.heading.toDouble()));
-           // telemetry.update();
+
+//END CODE FOR DRIVING
+
+//  KILL BUTTON*****************************************************************
+            if(gamepad2.back){
+                currentState = State.IDLE;
+                launchStep = 0;
+                shooter.shooterMotorLeft.setPower(0);
+                shooter.shooterMotorRight.setPower(0);
+            }
+//END KILL BUTTON************************************************************
+
+//CODE TO OPERATE INTAKE
+            //Operate Intake: left bumper is intake and right bumper is eject
+            if (gamepad2.left_bumper) {
+                intake.setPower(1);
+            } else if (gamepad2.right_bumper) {
+                intake.setPower(-1);//sets the power on the intake motor based on the values from the bumps
+            } else {
+                intake.setPower(0);
+            }
+// ---------------- INTAKE + CAROUSEL SEQUENCE (GAMEPAD 1 RIGHT TRIGGER) ----------------
+                /*
+                This section of the code is an intake sequence. It cycles 3 times.  Upon pulling the trigger
+                on gamepad 1, the intake begins spinning and the carousel spins to intakePositionOne.
+                On the second trigger pull, the intake spins and the carousel advances to intakePositionTwo.
+                On the third trigger pull, the intake spins and the carousel advances to intakePositionThree,
+                then resets for the next cycle.
+                */
+
+            boolean triggerPressed = gamepad1.right_trigger > 0.25;
+
+            // While trigger is held, keep intake running
+            if (triggerPressed) {
+                intake.setPower(intakePower);
+            }
+
+            // Detect NEW trigger pull (rising edge)
+            if (triggerPressed && !triggerHeld) {
+                triggerHeld = true;
+
+                // Advance carousel one position per pull
+                if (intakeStep == 0) {
+                    intake.setPower(intakePower);
+                    carousel.spinCarouselIntakeOne();
+                    intakeStep = 1;
+                } else if (intakeStep == 1) {
+                    intake.setPower(intakePower);
+                    carousel.spinCarouselIntakeTwo();
+                    intakeStep = 2;
+                } else if (intakeStep == 2) {
+                    intake.setPower(intakePower);
+                    carousel.spinCarouselIntakeThree();
+                    intakeStep = 0;
+                }
+            }
+
+            // Detect trigger release
+            if (!triggerPressed && triggerHeld) {
+                triggerHeld = false;
+                intake.setPower(0);   // Stop intake when released
+            }
+//END INTAKE SEQUENCE---------------------------------------
+/**
+ //MANUAL LOAD-----------------------------
+ if (ADD CONTROL HERE) {
+ carousel.setHomePositionKicker();
+ carousel.spinCarouselLaunchOne();
+ carousel.setTinyKicker();
+ }
+ //END MANUAL LOAD-----------------------------------------------
+ */
+
+//JUST FOR TESTING POSITON OF KICKER SERVO-------
+
+            if (gamepad2.b) {
+                carousel.setHomePositionKicker();
+            }
+            if (gamepad2.x) {
+                carousel.setTinyKicker();
+            }
+            if (gamepad2.y) {
+                carousel.setFullKicker();
+            }
+//JUST FOR TESTING POSITION OF TILT SERVO
+            if (gamepad1.a) {
+                shooter.setNearTiltPosition(shooter.nearTiltPosition);
+            }
+
+            if (gamepad1.b) {
+                shooter.setFarTiltPosition(shooter.farTiltPosition);
+            }
+
+            if (gamepad1.x) {
+                shooter.setHomeTiltPosition();
+            }
+
+//END CODE FOR TESTING POSITION OF TILT SERVO************************************
+
+//JUST FOR TESTING POSITION OF CAROUSEL********************************************
+            if (gamepad2.dpad_left) {
+                carousel.spinCarouselHome();
+            }
+
+            if (gamepad2.dpad_up) {
+                carousel.spinCarouselIntakeOne();
+            }
+
+            if (gamepad2.dpad_right) {
+                carousel.spinCarouselIntakeTwo();
+            }
+
+            if (gamepad2.dpad_down) {
+                carousel.spinCarouselIntakeThree();
+            }
+
+            if (gamepad1.dpad_up) {
+                carousel.spinCarouselLaunchOne();
+            }
+
+            if (gamepad1.dpad_right) {
+                carousel.spinCarouselLaunchTwo();
+            }
+
+            if (gamepad1.dpad_down) {
+                carousel.spinCarouselLaunchThree();
+            }
+//--------------------------END TESTING OF CAROUSEL
+
 
 
             TelemetryPacket packet = new TelemetryPacket();
